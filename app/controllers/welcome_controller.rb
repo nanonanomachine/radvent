d0907b4d42ba3404fdfee663b262ca53f30c452f
nanonanomachine <koyama.gakugai@gmail.com>
2014-11-08 02:54:13 +0900

    Add calendar view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,22 @@
 class WelcomeController < ApplicationController
   def index
+  	from_date = Date.new(2014, 12, 1).beginning_of_week(:sunday)
+  	to_date = Date.new(2014, 12, 31).end_of_week(:sunday)
+  	@calendar_data = split_week(from_date.upto(to_date))
+  end
+
+  private
+  def split_week(range)
+  	weeks = []
+  	week = []
+  		
+  	range.each do |date|
+  		week << date
+  		if date.wday == 6
+  			weeks << week
+  			week = []
+  		end
+  	end
+  	weeks << week
   end
 end
