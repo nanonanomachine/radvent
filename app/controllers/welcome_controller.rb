eb72acfac7347a9d1a9132a42cf1e27fddc85606
nanonanomachine <koyama.gakugai@gmail.com>
2014-11-09 15:35:07 +0900

    Show item links in calendar if the day has come

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,20 +4,21 @@ class WelcomeController < ApplicationController
   	to_date = Date.new(2014, 12, 31).end_of_week(:sunday)
   	@calendar_data = split_week(from_date.upto(to_date))
   	@advent_calendar_items =AdventCalendarItem.all
+  	@today = Date.today
   end
 
   private
-	  def split_week(range)
-	  	weeks = []
-	  	week = []
-	  		
-	  	range.each do |date|
-	  		week << date
-	  		if date.wday == 6
-	  			weeks << week
-	  			week = []
-	  		end
-	  	end
-	  	weeks << week
-	  end
+  def split_week(range)
+  	weeks = []
+  	week = []
+  		
+  	range.each do |date|
+  		week << date
+  		if date.wday == 6
+  			weeks << week
+  			week = []
+  		end
+  	end
+  	weeks << week
+  end
 end
