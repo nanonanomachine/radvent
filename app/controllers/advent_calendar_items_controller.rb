88efdd7d96738977a93d4a33e2b9649dc17bdd93
nanonanomachine <koyama.gakugai@gmail.com>
2014-11-09 12:36:29 +0900

    Add AdventCalendarItem edit action

--- app/controllers/advent_calendar_items_controller.rb
+++ app/controllers/advent_calendar_items_controller.rb
@@ -8,6 +8,23 @@ class AdventCalendarItemsController < ApplicationController
 		redirect_to new_item_path advent_calendar_item_id: advent_calendar_item.id
 	end
 
+	def show
+		@advent_calendar_item = AdventCalendarItem.find(params[:id])
+	end
+
+	def edit
+		@advent_calendar_item = AdventCalendarItem.find(params[:id])
+	end
+
+	def update
+		@advent_calendar_item = AdventCalendarItem.find(params[:id])
+		if @advent_calendar_item.update(advent_calendar_item_params)
+			render :edit, id: params[:id], notice: '更新しました'
+		else
+			render :edit
+		end
+	end
+
 	private 
 	def advent_calendar_item_params
 		params.require(:advent_calendar_item).permit(:user_name, :comment, :date)
