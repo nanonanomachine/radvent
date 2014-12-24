f1ec36a461232f4872414ca1a1da73104919ee6b
nanonanomachine <koyama.gakugai@gmail.com>
2014-12-24 23:52:56 +0900

    Remove test folder

--- test/controllers/attachments_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AttachmentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/helpers/advent_calendar_items_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class AdventCalendarItemsHelperTest < ActionView::TestCase
-end
