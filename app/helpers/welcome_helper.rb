83698407536c748bc6cdc10620c434ec3f1f2e8d
nanonanomachine <koyama.gakugai@gmail.com>
2015-01-25 19:30:45 +0900

    Add advent_calendar_date? method to WelcomeHelper which judges whether date is in advent calendar's date refs #7

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,7 @@
 module WelcomeHelper
+    def advent_calendar_date?(date)
+        date.month == Constants::MONTH  &&
+            date.day >= Constants::START_DAY &&
+            date.day <= Constants::END_DAY
+    end
 end
