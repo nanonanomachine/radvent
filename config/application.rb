a9b543a20889a033d0aa2d6baf993c097496675c
nanonanomachine <koyama.gakugai@gmail.com>
2015-02-26 22:41:18 +0900

    Set ja locale refs #25

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Radvent
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :ja
 
     config.generators do |g|
         g.test_framework :rspec,
