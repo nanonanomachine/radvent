78296e8fdfe6674115dad4a747f819bc6cbcb5ac
nanonanomachine <koyama.gakugai@gmail.com>
2015-11-08 05:14:44 +0900

    Use soft-tabs with a two space for AdventCalendarItem controller refs #28

--- app/controllers/advent_calendar_items_controller.rb
+++ app/controllers/advent_calendar_items_controller.rb
@@ -1,32 +1,32 @@
 class AdventCalendarItemsController < ApplicationController
-	def new
-		@advent_calendar_item = AdventCalendarItem.new(date: params[:date])
-	end
+  def new
+    @advent_calendar_item = AdventCalendarItem.new(date: params[:date])
+  end
 
-	def create
-		advent_calendar_item = AdventCalendarItem.create(advent_calendar_item_params)
-		redirect_to advent_calendar_item
-	end
+  def create
+    advent_calendar_item = AdventCalendarItem.create(advent_calendar_item_params)
+    redirect_to advent_calendar_item
+  end
 
-	def show
-		@advent_calendar_item = AdventCalendarItem.find(params[:id])
-	end
+  def show
+    @advent_calendar_item = AdventCalendarItem.find(params[:id])
+  end
 
-	def edit
-		@advent_calendar_item = AdventCalendarItem.find(params[:id])
-	end
+  def edit
+    @advent_calendar_item = AdventCalendarItem.find(params[:id])
+  end
 
-	def update
-		@advent_calendar_item = AdventCalendarItem.find(params[:id])
-		if @advent_calendar_item.update(advent_calendar_item_params)
-			redirect_to @advent_calendar_item
-		else
-			render :edit
-		end
-	end
+  def update
+    @advent_calendar_item = AdventCalendarItem.find(params[:id])
+    if @advent_calendar_item.update(advent_calendar_item_params)
+      redirect_to @advent_calendar_item
+    else
+      render :edit
+    end
+  end
 
-	private 
-	def advent_calendar_item_params
-		params.require(:advent_calendar_item).permit(:user_name, :comment, :date)
-	end
+  private 
+  def advent_calendar_item_params
+    params.require(:advent_calendar_item).permit(:user_name, :comment, :date)
+  end
 end
