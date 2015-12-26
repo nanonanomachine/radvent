1bd091b985df7a84276845878add4c628639ef78
nanonanomachine <koyama.gakugai@gmail.com>
2015-12-26 21:10:10 +0900

    Add rspec test which asserts returning title  if Item has title value #32

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe Item do
+  it "retuns title value if title is not empty" do
+    item = build(:item, title: "title")
+    expect(item.title).to eq("title")
+  end
+
   it "returns deafult value if title is empty" do
     item = build(:item, title: "")
     expect(item.title).to eq(I18n.t("activerecord.attributes.item.title_empty"))
